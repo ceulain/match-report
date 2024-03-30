2dfae357a154035274c0b5eca97f69595c1d3062
Ceulain <ceul1barth@hotmail.fr>
2024-03-30 15:41:26 +0100

    feat: update match input

--- src/MatchInput/index.tsx
+++ src/MatchInput/index.tsx
@@ -3,16 +3,26 @@ import classes from "./styles.module.css";
 const MatchInput = () => {
   return (
     <div className={classes.wrapper}>
-      <form>
-        {/* <label htmlFor="me">Quelle est le nom de votre équipe ?</label> */}
-        <input type="text" name="me" id="me" />
+      {/* <label htmlFor="me">Quelle est le nom de votre équipe ?</label> */}
+      <input type="text" name="me" id="me" placeholder="Nom de votre équipe" />
 
-        {/* <label htmlFor="opponent">Quelle est votre adversaire ?</label> */}
-        <input type="text" name="opponent" id="opponent" />
+      {/* <label htmlFor="opponent">Quelle est votre adversaire ?</label> */}
+      <input
+        type="text"
+        name="opponent"
+        id="opponent"
+        placeholder="Nom de votre adversaire"
+      />
 
-        {/* <label htmlFor="match-date">Quelle est la date de votre match ?</label> */}
-        <input type="datetime-local" name="match-date" id="match-date" />
+      {/* <label htmlFor="match-date">Quelle est la date de votre match ?</label> */}
+      <input
+        type="datetime-local"
+        name="match-date"
+        id="match-date"
+        placeholder="Date de votre match ?"
+      />
 
+      <span>
         <label htmlFor="location">Ou se joue votre match ?</label>
         <select name="location" id="location">
           <option selected value="Domicile">
@@ -20,7 +30,7 @@ const MatchInput = () => {
           </option>
           <option value="Exterieur">Exterieur</option>
         </select>
-      </form>
+      </span>
     </div>
   );
 };
