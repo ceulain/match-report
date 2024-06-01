343326a30ca177baef82560d0e8c8a60e5caa247
Ceulain <ceul1barth@hotmail.fr>
2024-06-01 23:49:09 +0200

    feat: add background to modal

--- src/Modal/content.tsx
+++ src/Modal/content.tsx
@@ -7,14 +7,16 @@ type Props = {
 export default function Content({ onClose }: Props) {
   return (
     <div className={classes.wrapper}>
-      <span
-        className={`material-symbols-outlined ${classes["close-button"]}`}
-        onClick={onClose}
-      >
-        close
-      </span>
-      <p>Je suis une modal</p>
-      <button onClick={onClose}>Valider</button>
+      <div className={classes["modal-wrapper"]}>
+        <span
+          className={`material-symbols-outlined ${classes["close-button"]}`}
+          onClick={onClose}
+        >
+          close
+        </span>
+        <p>Je suis une modal</p>
+        <button onClick={onClose}>Valider</button>
+      </div>
     </div>
   );
 }
